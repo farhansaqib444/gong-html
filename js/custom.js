0b6bf6c8dc941c7028de06dcc0a852b7ddb9246b
bendevjb <bendev@jobesk.com>
2020-08-21 22:59:45 +0500

    privacy.html

--- js/custom.js
+++ js/custom.js
@@ -52,3 +52,20 @@
     }
   });
 });
+
+
+
+  // Upload stroy images Start
+
+
+  $(document).ready(function(){
+    $('.upload-wrap input[type=file]').change(function(){
+      var id = $(this).attr("id");
+      var newimage = new FileReader();
+      newimage.readAsDataURL(this.files[0]);
+      newimage.onload = function(e){
+        $('.uploadpreview.' + id ).css('background-image', 'url(' + e.target.result + ')' );
+      }
+    });
+  });
+  // Upload stroy images End
